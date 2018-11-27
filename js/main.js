c5060032df05cec0a27cbb692282872962aac84c
Unknown <coen_walter@hotmail.com>
2018-11-27 16:56:58 +0100

    Added tilt.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,32 @@
-document.addEventListener('DOMContentLoaded', function(){
+$(document).ready(function () {
 
-    
-    
+
+    $('img').each(function () {
+
+
+
+        $(this).wrap($('<a/>', {
+            href: $(this).attr('src'),
+            'data-fancybox': 'gallery',
+            class: "fancybox",
+            rel: "artikel"
+        }));
+    });
+
+
+
+
+    $('[data-fancybox="gallery"]').fancybox({
+        arrows: false,
+        toolbar: true,
+        infobar: false,
+        buttons:["close"]
+    });
+
+
+    $('.js-tilt').tilt({
+    reset: false
+    });
     
 
 });
